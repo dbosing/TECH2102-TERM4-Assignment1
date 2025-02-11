c709e7baaaaac58638b63dd026951f5c830842d6
dbosing <d.bosing154@mybvc.ca>
2025-02-11 14:40:57 -0700

    Add files via upload

---
+++ Assignment1/connection.php
@@ -0,0 +1,13 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "secret";
+$dbname = "students";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("". $conn->connect_error);
+}
+
+?>
